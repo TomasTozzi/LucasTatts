8447029cdd096c4ab8fc8bcd85e7c1b1d8e4d67e
TomasTozzi <tozzitomas@gmail.com>
2024-08-29 21:11:41 -0300

    pequeño arreglo en  las categorias

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ const Card = () => {
   return (
     <div
       className="grid grid-cols-2 gap-2 p-2
-        sm:grid-cols-1 sm:gap-2
+        sm:grid-cols-1 sm:gap-2 
         md:w-[60%] md:mx-auto md:h-[30vh]
-        md:grid-cols-4 md:w-[100%]  lg:w-[90%] lg:h-[25vh] lg:gap-4"
+        md:grid-cols-4 md:w-[80vw]  lg:w-[90%] lg:h-[25vh] lg:gap-4 border-black border-2"
     >
       {CategoriasJson.categorias.map((categoria) => (
         <div
@@ -18,7 +18,7 @@ const Card = () => {
         >
           <div className="relative w-full h-full">
             {/* Background Image */}
-            <div className="w-full h-[25vh] bg-cover bg-home sm:h-[40vh]"></div>
+            <div className="w-full h-[40vh] bg-cover bg-home sm:h-[40vh]"></div>
             {/* Overlay with Text */}
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
               <h2 className="text-white text-lg font-bold">{categoria.name}</h2>
