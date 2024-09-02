616c2f3e5de2a0bd3de678308686e5b20c8df2d2
TomasTozzi <tozzitomas@gmail.com>
2024-09-02 18:34:53 -0300

    fix al nav ya parece el fondo al abrir

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ function NavBar() {
   };
 
   return (
-    <header id="home" className={`transition-colors duration-300 ${scrolled ? 'bg-black bg-opacity-80' : 'bg-transparent'}`}>
+    <header id="home" className={`transition-colors duration-300 ${scrolled || isOpen ? 'bg-black bg-opacity-80' : 'bg-transparent'}`}>
       <nav className={`border-gray-200 dark:border-gray-900 ${scrolled ? 'bg-black bg-opacity-80' : 'bg-transparent'}`}>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
@@ -32,7 +32,7 @@ function NavBar() {
           <button
             onClick={toggleMenu}
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center  p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded={isOpen}
           >
@@ -53,7 +53,7 @@ function NavBar() {
               />
             </svg>
           </button>
-          <div className={`w-full md:block md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-default">
+          <div className={`w-full md:block md:w-auto ${isOpen ? 'block ' : 'hidden'}`} id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
