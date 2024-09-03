430a7273bae673ce13a9e4103360a1c285cab022
TomasTozzi <tozzitomas@gmail.com>
2024-09-02 23:18:13 -0300

    se agrego el mapa, mirar el respondive

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,3 +1,4 @@
+import Mapa from "../components/Mapa";
 import { useState } from "react";
 
 const ContactForm = () => {
@@ -22,69 +23,84 @@ const ContactForm = () => {
   };
 
   return (
-    <div id="contacto" className="w-full mx-auto p-8 bg-white rounded-lg shadow-lg md:w-[75%] lg:w-[40%] my-[5vh] border border-gray-200">
-      <h2 className="text-3xl font-semibold mb-6 text-center text-gray-900">
-        Contáctanos
-      </h2>
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <div className="relative">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-800"
-          >
-            Nombre
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
-            required
-          />
+    <div className="flex flex-col-reverse lg:flex-row md:pb-4 items-center">
+      <div className="w-full  mx-auto p-8 bg-white rounded-lg shadow-lg md:w-[75%] h-screen h-[58vh]  lg:w-[40%] ">
+        <h2 className="text-3xl font-semibold mb-6 text-center text-gray-900">
+          Podes encontrarnos en
+        </h2>
+        <div className="h-full ">
+          <Mapa />
         </div>
-        <div className="relative">
-          <label
-            htmlFor="phone"
-            className="block text-sm font-medium text-gray-800"
+      </div>
+      <div
+        id="contacto"
+        className="w-full mx-auto p-8 bg-white rounded-lg shadow-lg md:w-[75%] h-screen h-[70%]  lg:w-[40%] my-[5vh] border border-gray-200"
+      >
+        <h2 className="text-3xl font-semibold mb-6 text-center text-gray-900">
+          Contáctanos
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="flex flex-wrap space-y-6 md:space-y-0 md:space-x-4">
+            <div className="relative flex-grow md:flex-1">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-800"
+              >
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
+                required
+              />
+            </div>
+            <div className="relative flex-grow md:flex-1">
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium text-gray-800"
+              >
+                Número
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
+                required
+              />
+            </div>
+          </div>
+          <div className="relative">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-800"
+            >
+              Mensaje
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows="5"
+              className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 px-4 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
           >
-            Número
-          </label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
-            required
-          />
-        </div>
-        <div className="relative">
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-800"
-          >
-            Mensaje
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            rows="5"
-            className="mt-2 block w-full border border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-500 focus:ring-opacity-50 px-4 py-3"
-            required
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="w-full py-3 px-4 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-        >
-          Enviar
-        </button>
-      </form>
+            Enviar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ src/components/Mapa.jsx
@@ -0,0 +1,15 @@
+function Mapa() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.5386873134107!2d-58.190691741599046!3d-34.789511253743356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a3290024e944f5%3A0x6ef0d97903f4c677!2sLucas%20Tattoo!5e0!3m2!1ses-419!2sar!4v1725324490113!5m2!1ses-419!2sar"
+      width="100%"
+      className=" h-screen h-[40vh] sm:h-[80%] lg:h-[80%] xl:h-[80%]" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+}
+
+export default Mapa;
